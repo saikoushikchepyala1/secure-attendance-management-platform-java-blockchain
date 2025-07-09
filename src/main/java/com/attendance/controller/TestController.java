e4625634106a205bfd77799856818589faecd4b6
Sai Koushik <saikoushikchepyala@gmail.com>
2025-07-09 21:26:58 +0530

    path updated

--- src/main/java/com/attendance/controller/TestController.java
+++ src/main/java/com/attendance/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/api/test")
+    @GetMapping("/")
     public String test() {
         return "Backend working!";
     }
