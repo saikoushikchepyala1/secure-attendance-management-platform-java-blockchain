412b46834356a0f4221a14261ee701c903455d76
Sai Koushik <saikoushikchepyala@gmail.com>
2025-07-16 17:28:37 +0530

    updated securityconfig file

--- src/main/java/com/attendance/security/SecurityConfig.java
+++ src/main/java/com/attendance/security/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig {
                                 "http://127.0.0.1:5500",
                                 "http://localhost:5500",
                                 "http://localhost:63342",
-                                "http://127.0.0.1:63342"
+                                "http://127.0.0.1:63342",
+                                "https://effulgent-nougat-125426.netlify.app/"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
